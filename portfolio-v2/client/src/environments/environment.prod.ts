26582830c947bf061a370e3dd6c965c417035f0a
Tharun <tharunummadala@gmail.com>
2026-07-12 19:09:14 +0530

    Configure active Web3Forms access key in environment files

--- portfolio-v2/client/src/environments/environment.prod.ts
+++ portfolio-v2/client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://server-drab-five-60.vercel.app/api',
-  web3FormsKey: 'YOUR_WEB3FORMS_ACCESS_KEY'
+  web3FormsKey: '58c4018f-ffb9-4e91-bb97-1c19d986fecd'
 };
--- portfolio-v2/client/src/environments/environment.ts
+++ portfolio-v2/client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api',
-  web3FormsKey: 'YOUR_WEB3FORMS_ACCESS_KEY'
+  web3FormsKey: '58c4018f-ffb9-4e91-bb97-1c19d986fecd'
 };
